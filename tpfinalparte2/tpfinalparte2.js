67815ebaf85d9185153eb34272d1517a65f759aa
victoria di lorenzo <128644257+vickydl19@users.noreply.github.com>
2024-11-14 17:31:52 -0300

    Add files via upload

---
+++ tpfinalparte2/tpfinalparte2.js
@@ -0,0 +1,20 @@
+//Victoria Di Lorenzo y Azul Ceballos
+//Comisión 1
+//link a video 
+let juego;
+let sonidoFondo;
+
+function preload() {
+  fondoInicio = new Fondo("data/imagen5.jpeg");
+  fondoJuego = new Fondo("data/imagen.jpg");
+  sonidoFondo = loadSound("data/fondo.mp3");
+}
+
+function setup() {
+  createCanvas(640, 480);
+  juego = new Juego(); 
+}
+
+function draw() {
+  juego.mostrar();  
+}
